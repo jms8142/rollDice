3086fb93233fa6d789f9d6437950a9f16a15788f
John Skrzypek <jms8142@hotmail.com>
2022-01-30 20:26:35 -0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 
 const App = () => {
-  const [activity, setActivity] = useState();
-  const [showDice, setShowDice] = useState(false);
+  const [activity, setActivity] = useState("");
+  const [showDice, setShowDice] = useState(true);
   const [message, setMessage] = useState("");
   const [firstRoll, setFirstRoll] = useState(true);
-
+  const [showButton, setShowButton] = useState(false);
   const styles = {
     dice: {
       display: showDice ? "inline-block" : "none"
@@ -17,37 +17,48 @@ const App = () => {
     try {
       const response = await fetch("https://www.boredapi.com/api/activity");
       const data = await response.json();
-      setActivity(data.activity);
+      setTimeout(() => {
+        setActivity(data.activity);
+      }, 2000);
     } catch (e) {
       console.log(e);
     }
   };
 
+  useEffect(() => {
+    if (activity) {
+      setShowDice(false);
+      setMessage("");
+    }
+  }, [activity]);
+
   const rollDice = () => {
+    setActivity("");
+    setShowButton(false);
     setShowDice(true);
     if (firstRoll) {
       setFirstRoll(false);
     }
     getData();
     setMessage("Thinking..");
-    // setTimeout(() => setMessage("Still thinking.."), 1000);
-    setMessage("");
-    setShowDice(false);
+    setTimeout(() => setMessage("Still thinking.."), 1000);
   };
 
   return (
     <div className="App">
       <h1>What should I do today?</h1>
-      <button onClick={rollDice}>
-        {firstRoll ? "Roll Dice" : "Roll Again"}
-      </button>
+      {showButton && (
+        <button onClick={rollDice}>
+          {firstRoll ? "Roll Dice" : "Roll Again"}
+        </button>
+      )}
       <img
         style={styles.dice}
         width="100"
         alt="dice"
         src="https://i0.wp.com/www.richardhughesjones.com/wp-content/uploads/2019/01/dice-gif.gif"
       />
-      {message}
+      {message} Thinking
       <h2>{activity}</h2>
     </div>
   );
